8a6f9a10ec6242e3d5b697ee4c86116ed5c8899a
Erik Peterson <erik.peterson@acquia.com>
2018-03-30 15:54:11 +0000

    Adding redirect of output to db-backup.sh

--- scripts/db-backup.sh
+++ scripts/db-backup.sh
@@ -5,25 +5,25 @@ HOMEDIR=/home/eporama
 LOGPATH=$HOMEDIR/cronlogs
 BACKUPPATH=$HOMEDIR/backups
 
-echo "=================="
-echo "Running backup: ${RUNTIME}"
+echo "==================" >> ${LOGPATH}/db-backups.log 2>&1
+echo "Running backup: ${RUNTIME}" >> ${LOGPATH}/db-backups.log 2>&1
 
 if [ ! -f $LOGPATH ]; then
-  echo "creating logpath directory"
+  echo "creating logpath directory" >> ${LOGPATH}/db-backups.log 2>&1
   mkdir -p $LOGPATH
 fi
 
 if [ ! -d $BACKUPPATH ]; then
-  echo "creating backuppath directory"
+  echo "creating backuppath directory" >> ${LOGPATH}/db-backups.log 2>&1
   mkdir -p $BACKUPPATH
 fi
 
 # Create a gzipped sql dump.
-echo "running sql-dump"
+echo "running sql-dump" >> ${LOGPATH}/db-backups.log 2>&1
 /var/www/eporama_web/vendor/bin/drush sql-dump --structure-tables-key=common --root=/var/www/eporama_web/web --uri=eporama.com  --gzip --result-file=${BACKUPPATH}/eporama-$(date +%F).sql >> ${LOGPATH}/db-backups.log 2>&1
 
 # Send the backup to S3 for safe keeping
-echo "sending backup to s3cmd"
+echo "sending backup to s3cmd" >> ${LOGPATH}/db-backups.log 2>&1
 s3cmd --no-progress --no-guess-mime-type put $BACKUPPATH/eporama-${RUNDATE}.sql.gz s3://eporama >> ${LOGPATH}/db-backups.log 2>&1
 
-echo ""
+echo "" >> ${LOGPATH}/db-backups.log 2>&1
