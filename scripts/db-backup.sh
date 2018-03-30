e202ea92a7dcdbc50eaca1ba65d4337642dad96c
Erik Peterson <erik.peterson@acquia.com>
2018-03-30 15:40:57 +0000

    Updating db-backup perms and drush path

--- scripts/db-backup.sh
+++ scripts/db-backup.sh
@@ -12,7 +12,7 @@ if [ ! -d $BACKUPPATH ]; then
 fi
 
 # Create a gzipped sql dump.
-/var/www/eporama_web/vendor/bin/drush sql-dump --structure-tables-key=common --root=/var/www/eporama_web/web --uri=eporama.com  --gzip --result-file=${BACKUPPATH}/eporama-$(date +%F).sql >> ${LOGPATH}/db-backups.log 2>&1
+/var/www/eporama_web/vendor/drush/drush/drush sql-dump --structure-tables-key=common --root=/var/www/eporama_web/web --uri=eporama.com  --gzip --result-file=${BACKUPPATH}/eporama-$(date +%F).sql >> ${LOGPATH}/db-backups.log 2>&1
 
 # Send the backup to S3 for safe keeping
 s3cmd --no-progress --no-guess-mime-type put $BACKUPPATH/eporama-$(date +%F).sql.gz s3://eporama >> ${LOGPATH}/db-backups.log 2>&1
