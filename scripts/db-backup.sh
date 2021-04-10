7660b38cf680a9afa68bcb26994e0e7d7a5530c4
Erik Peterson <eporama@gmail.com>
2021-04-09 22:50:43 -0400

    Updating backup script

--- scripts/db-backup.sh
+++ scripts/db-backup.sh
@@ -4,26 +4,27 @@ RUNTIME=$(date)
 HOMEDIR=/home/eporama
 LOGPATH=$HOMEDIR/cronlogs
 BACKUPPATH=$HOMEDIR/backups
+LOG=$LOGPATH/db-backups.log
 
-echo "==================" >> ${LOGPATH}/db-backups.log 2>&1
-echo "Running backup: ${RUNTIME}" >> ${LOGPATH}/db-backups.log 2>&1
+echo "==================" >> ${LOG} 2>&1
+echo "Running backup: ${RUNTIME}" >> ${LOG} 2>&1
 
 if [ ! -d $LOGPATH ]; then
-  echo "creating logpath directory" >> ${LOGPATH}/db-backups.log 2>&1
+  echo "creating logpath directory" >> ${LOG} 2>&1
   mkdir -p $LOGPATH
 fi
 
 if [ ! -d $BACKUPPATH ]; then
-  echo "creating backuppath directory" >> ${LOGPATH}/db-backups.log 2>&1
+  echo "creating backuppath directory" >> ${LOG} 2>&1
   mkdir -p $BACKUPPATH
 fi
 
 # Create a gzipped sql dump.
-echo "running sql-dump" >> ${LOGPATH}/db-backups.log 2>&1
-/var/www/eporama_web/vendor/bin/drush sql-dump --structure-tables-key=common --root=/var/www/eporama_web/web --uri=eporama.com  --gzip --result-file=${BACKUPPATH}/eporama-$(date +%F).sql >> ${LOGPATH}/db-backups.log 2>&1
+echo "running sql-dump" >> ${LOG} 2>&1
+/var/www/eporama_web/vendor/bin/drush sql-dump --structure-tables-key=common --root=/var/www/eporama_web/web --uri=eporama.com  --gzip --result-file=${BACKUPPATH}/eporama-$(date +%F).sql >> ${LOG} 2>&1
 
 # Send the backup to S3 for safe keeping
-echo "sending backup to s3cmd" >> ${LOGPATH}/db-backups.log 2>&1
-s3cmd --no-progress --no-guess-mime-type put $BACKUPPATH/eporama-${RUNDATE}.sql.gz s3://eporama >> ${LOGPATH}/db-backups.log 2>&1
+echo "sending backup to s3cmd" >> ${LOG} 2>&1
+s3cmd --no-progress --no-guess-mime-type put $BACKUPPATH/eporama-${RUNDATE}.sql.gz s3://eporama >> ${LOG} 2>&1
 
-echo "" >> ${LOGPATH}/db-backups.log 2>&1
+echo "" >> ${LOG} 2>&1
