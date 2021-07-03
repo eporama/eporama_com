80cf1c84529bf8f1d1ef409690117728329321af
Erik Peterson <eporama@gmail.com>
2021-07-03 15:44:36 -0400

    Fixing backup script web path

--- scripts/db-backup.sh
+++ scripts/db-backup.sh
@@ -2,6 +2,7 @@
 RUNDATE=$(date +%F)
 RUNTIME=$(date)
 HOMEDIR=/home/eporama
+WEBDIR=/var/www/eporama_com
 LOGPATH=$HOMEDIR/cronlogs
 BACKUPPATH=$HOMEDIR/backups
 LOG=$LOGPATH/db-backups.log
@@ -21,7 +22,7 @@ fi
 
 # Create a gzipped sql dump.
 echo "running sql-dump" >> ${LOG} 2>&1
-/var/www/eporama_web/vendor/bin/drush sql-dump --structure-tables-key=common --root=/var/www/eporama_web/web --uri=eporama.com  --gzip --result-file=${BACKUPPATH}/eporama-$(date +%F).sql >> ${LOG} 2>&1
+${WEBDIR}}/vendor/bin/drush sql-dump --structure-tables-key=common --root=${WEBDIR}}/web --uri=eporama.com  --gzip --result-file=${BACKUPPATH}/eporama-$(date +%F).sql >> ${LOG} 2>&1
 
 # Send the backup to S3 for safe keeping
 echo "sending backup to s3cmd" >> ${LOG} 2>&1
